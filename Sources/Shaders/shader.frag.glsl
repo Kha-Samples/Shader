7c1f832339cb02e626ba35e7e265cf013b75c664
Robert Konrad <git2016@robdangero.us>
2017-05-12 18:11:22 +0200

    Update Kha

--- Sources/Shaders/shader.frag.glsl
+++ Sources/Shaders/shader.frag.glsl
@@ -1,3 +1,7 @@
+#version 450
+
+out vec4 FragColor;
+
 void main() {
-	gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+	FragColor = vec4(1.0, 0.0, 0.0, 1.0);
 }
--- Sources/Shaders/shader.vert.glsl
+++ Sources/Shaders/shader.vert.glsl
@@ -1,4 +1,6 @@
-attribute vec3 pos;
+#version 450
+
+in vec3 pos;
 
 void main() {
 	gl_Position = vec4(pos.x, pos.y, 0.5, 1.0);
